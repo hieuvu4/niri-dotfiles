8d39f2b7da746e970a25326ef1cbac84ad0c3045
Nguyen Duc Hieu Vu <ndhieuv@gmx.net>
2026-01-30 17:23:33 +0100

    added pywal for discord and pywal

--- README.md
+++ README.md
@@ -1,5 +1,72 @@
 # my-niri-dotfiles
 
 ```
-sudo pacman -S niri kitty vim neovim git xorg-xwayland xwayland-satellite xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber ttf-jetbrains-mono-nerd swww nwg-look python-pywal python-pillow v4l2loopback-dkms jq dbus
+sudo pacman -S git gnome-keyring  pipewire-pulse pipewire-alsa pipewire-jack  nwg-look v4l2loopback-dkms jq dbus
+```
+
+
+
+
+
+# Essentials
+
+```
+sudo pacman -Syu niri xorg-xwayland xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk waybar swww pipewire wireplumber pulsemixer
+```
+
+```
+sudo pacman -S kitty htop unzip vim neovim ranger  python-pillow python-pywal fastfetch
+```
+
+
+## Fonts
+```
+sudo pacman -S noto-fonts ttf-jetbrains-mono-nerd
+```
+
+## Pywal for Firefox and Spotify-Launcher 
+
+```
+sudo pacman -S firefox spotify-launcher
+```
+
+
+```
+yay -S python-pywalfox yay -S spicetify-cli
+```
+```
+yay -S spicetify-cli
+```
+
+```
+curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
+```
+
+```
+spicetify backup apply
+```
+
+```
+spicetify update
+```
+
+
+# Gaming
+
+```
+sudo pacman -S steam 
+```
+
+```
+sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils
+```
+
+## Discord (Vesktop)
+
+```
+ln -s $HOME/.cache/wal/vesktop.css $HOME/.config/vesktop/themes/pywal-vesktop.theme.css
+```
+
+```
+ls -l ~/.config/vesktop/themes/pywal.theme.css
 ```
--- scripts/wallpaper-picker.sh
+++ scripts/wallpaper-picker.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-# Pfade definieren
+# Pfade
 CONFIG="$HOME/.config/niri/config.kdl"
 WALL_DIR="$HOME/.config/wallpaper"
 COLOR_CACHE="$HOME/.cache/wal/colors"
+SPICE_THEME="$HOME/.config/spicetify/Themes/wal"
 
-# 1. Ranger Auswahl
+# 1. Bild mit Ranger aussuchen
 TMP_FILE=$(mktemp)
 ranger --choosefile="$TMP_FILE" "$WALL_DIR"
 SELECTED=$(cat "$TMP_FILE")
 rm "$TMP_FILE"
 
-# Abbrechen wenn leer
 [ -z "$SELECTED" ] && exit 0
 
-# 2. Wallpaper setzen
+# 2. Wallpaper setzen mit swww
 swww img "$SELECTED" --transition-type wipe --transition-duration 2 --transition-fps 100
 
-# 3. Pywal Farben generieren
-wal -i "$SELECTED" -n -q -s
+# 3. Pywal Farben extrahieren
+# -q für quiet, -n für kein Setzen des Xresources (da wir Niri/Wayland nutzen)
+wal -i "$SELECTED" -n -q
 
-# 4. Farben aus Pywal Cache extrahieren (Zeile 3 für Aktiv, Zeile 1 für Inaktiv)
+# 4. Themes aktualisieren
+# Spicetify: Watcher im Hintergrund erkennt die Änderung an color.ini sofort
+cp "$HOME/.cache/wal/spicetify" "$SPICE_THEME/color.ini"
+
+# Vesktop: Datei berühren, damit der CSS-Import neu geladen wird
+touch "$HOME/.config/vesktop/themes/pywal.theme.css"
+
+# Browser (Firefox via Pywalfox)
+pywalfox update
+
+# 5. Niri Farben extrahieren (Zeile 3 Aktiv, Zeile 1 Inaktiv)
 RAW_ACT=$(sed -n '3p' "$COLOR_CACHE" | tr -d '[:space:]#')
 RAW_INACT=$(sed -n '1p' "$COLOR_CACHE" | tr -d '[:space:]#')
 
-# 5. In Niri Config schreiben und live laden
+# 6. In Niri Config schreiben
 if [ -n "$RAW_ACT" ]; then
     COLOR_ACT="#$RAW_ACT"
     COLOR_INACT="#$RAW_INACT"
     
-    # Hier passiert die Magie: sed sucht nach dem // {active} Kommentar
     sed -i "s|active-color \".*\" // {active}|active-color \"$COLOR_ACT\" // {active}|g" "$CONFIG"
     sed -i "s|inactive-color \".*\" // {inactive}|inactive-color \"$COLOR_INACT\" // {inactive}|g" "$CONFIG"
     
-    # Niri anweisen, die Config neu zu lesen
     niri msg action load-config-file "$CONFIG"
 fi
